6217811b59fa8f3358df9f9bef2daeaacc1b5930
Dicias <dmacias26@alumnos.uaq.mx>
2024-07-29 11:59:06 -0600

    v1

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    AuthModule,
+    MongooseModule.forRoot(process.env.MONGO_URI, {
+      dbName: process.env.MONGO_DB_NAME
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {
+
+
+}
